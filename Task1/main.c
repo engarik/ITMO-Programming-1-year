e356cb84df83e58baf525ca5d061b0e3861e9f3a
engarik <kornik36@gmail.com>
2020-09-11 21:06:20 +0300

    Structure and main page update

--- Task1/main.c
+++ Task1/main.c
@@ -1,14 +1,14 @@
-/* Kornilov Nikita, M3102, 10.09.2020, var. 8*/
+/* Kornilov Nikita, M3102, 10.09.2020, lab 1, var. 8*/
 #include <stdio.h>
 
 void main( void ) {
 	int integer = 0, input;
 	char character = 0;
-	
+
 	printf("What would you like to input? 1 - integer, 2 - character?\n");
 	scanf("%i", &input);
 	getchar();
-	
+
 	if (input == 1)
 	{
 		printf("Input your integer: ");
@@ -22,4 +22,3 @@ void main( void ) {
 		printf("Your input character was: %c\n", character);
 	}
 }
-
